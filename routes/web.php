463827c30160e10b1a11a6a78426f657a70eb9e6
Blake Nahin <bnahin@live.com>
2018-03-26 00:17:06 -0500

    Start VATSIM SSO

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('pages.main');
 });
+
+Route::get('/login', function () {
+    $send = "";
+    $returnUrl = "http://realops.test/validate";
+    VatsimSSO::login(
+        $returnUrl, function ($key, $secret, $url) {
+        Session::put('vatsimAuth', compact('key', 'secret'));
+        header('Location: '. $url);
+    }, function ($e) {
+        throw $e;
+    });
+});
+Route::get('/validate', function () {
+    $session = Session::get('vatsimAuth');
+
+    VatsimSSO::validate(
+        $session['key'], $session['secret'],
+        Request::input('oauth_verifier'), function ($user, $request) {
+        Session::forget('vatsimAuth');
+
+        dd($user);
+
+    });
+});
