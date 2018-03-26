884f376725ddd9acf5d978225043a5cf9bc6baba
Blake Nahin <bnahin@live.com>
2018-03-25 21:59:07 -0500

    Add favicon

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
 
 <title>{{ env('APP_NAME') }}</title>
+
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
 <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
